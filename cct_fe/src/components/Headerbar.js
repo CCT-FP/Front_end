272235b057367a5289318b4fe5cef300297690f9
조아빈 <whdkqls122@naver.com>
2023-09-09 14:46:48 +0900

    헤더바 거의 완성

--- cct_fe/src/components/Headerbar.js
+++ cct_fe/src/components/Headerbar.js
@@ -12,20 +12,20 @@ export default function Headerbar(){
                         로고
                         {/*로고이미지 backgrondimg로 할듯 아니면 그냥 글씨*/}
                     </div>
-                    <div className="headerbar-mypage"><Link to={'/mypage'}><BsPersonCircle/></Link></div>
+                    <div className="headerbar-mypage"><Link  className="link" to={'/mypage'}><BsPersonCircle size={40}/></Link></div>
                 </header>
                 <div className="headerbar-navigation">
-                    <div>
-                        <div className="headerbar-navigation__signbox">
-                            <button className="headerbtn headerbar-navigation__loginbtn"><Link to={'/loginpage'}>로그인</Link></button>
-                            /
-                            <button className="headerbtn headerbar-navigation__joinbtn"><Link to={'/joinpage'}>회원가입</Link></button>
-                        </div>
+                    <div className="headerbar-navigation__box">
                         <div className="headerbar-navigation__navigationbtn">
-                            <button className="headerbtn headerbar-navigation__navigationbtn--freelancer"><Link to={'/freelancerpage'}>개발자</Link></button>
-                            <button className="headerbtn headerbar-navigation__navigationbtn--company"><Link to={'/'}>회사</Link></button>
+                            <button className="headerbtn headerbar-navigation__navigationbtn--freelancer"><Link className="navigationlink" to={'/freelancerpage'}>개발자</Link></button>
+                            <button className="headerbtn headerbar-navigation__navigationbtn--company"><Link className="navigationlink" to={'/'}>회사</Link></button>
                         </div>
-                    </div>
+                        <div className="headerbar-navigation__signbox">
+                            <button className="headerbtn headerbar-navigation__loginbtn"><Link className="navigationlink" to={'/loginpage'}>로그인</Link></button>
+                            /
+                            <button className="headerbtn headerbar-navigation__joinbtn"><Link className="navigationlink" to={'/joinpage'}>회원가입</Link></button>
+                        </div>
+                    </div>    
                 </div>
             </div>
         </>
--- cct_fe/src/pages/Joinpage.js
+++ cct_fe/src/pages/Joinpage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import '../css/Joinpage.css'
 
 export default function Joinpage () {
     return(
-        <>
-            회원가입페이지
-        </>
+        <div className="joinbox">
+            <div className="joinbox-titlebox">
+                <h1 className="joinbox-titlebox__title">회원가입</h1>
+            </div>
+            <div className="joinbox-countbox">
+                <h3 className="joinbox-countbox__countpage">1 / 3</h3>
+            </div>
+        </div>
     )
 }
