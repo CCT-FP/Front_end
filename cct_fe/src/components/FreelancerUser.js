438196c3bf2346f367dfa03ea6283d5125e02892
조아빈 <whdkqls122@naver.com>
2023-09-12 00:29:32 +0900

    임채륜

--- cct_fe/src/components/FreelancerUser.js
+++ cct_fe/src/components/FreelancerUser.js
@@ -1,15 +1,111 @@
-import React, { useEffect } from "react";
-import '../css/Freelancerpage.css'
+import React, { useEffect, useState } from "react";
+import '../css/FreelancerUser.css'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
-export default function FreelancerUser (){
-   useEffect(()=>{
-        console.log('프리랜서') 
-   },[]) 
-   return(
-      <>
-      <h1 className="i">프리랜서임!</h1>
+export default function FreelancerUser(){
 
-      </>
-   )
+    const [userId, setUserId] = useState('')
+    const [userPw, setUserPw] = useState('')
+    const [userName, setUserName] = useState('')
+    const [userPwchk, setUserPwchk] = useState('')
+    const [userEmail, setUserEamil] = useState('')
+    const [userPhone, setUserPhone] = useState('')
+    const [userBirth, setUserBirth] = useState('')
+    const UserInfo = {}
+    const navigate = useNavigate()
 
+    useEffect(()=>{
+        console.log('컴퍼니')
+    },[])
+    const InputName = e => {
+        setUserName(e.target.value)
+    }
+    const InputId = e => {
+        setUserId(e.target.value)
+    }
+    const InputPw = e => {
+        setUserPw(e.target.value)
+    }
+    const InputPwchk = e => {
+        setUserPwchk(e.target.value)
+    }
+    const InputEmail = e => {
+        setUserEamil(e.target.value)
+    }
+    const InputPhonenum = e => {
+        setUserPhone(e.target.value)
+    }
+    const InputBirth = e => {
+        setUserBirth(e.target.value)
+    }
+    const JoinComplete = e => {
+        e.preventDefault()
+        UserInfo['id'] = userId
+        UserInfo['name'] = userName
+        UserInfo['password'] = userPw
+        UserInfo['email'] = userEmail
+        UserInfo['phone'] = userPhone
+        UserInfo['birth'] = userBirth
+        UserInfo['role'] = '프리랜서 선택'
+        console.log(UserInfo)
+
+        axios({
+            method : 'post',
+            url : '//localhost:8080/users/join',
+            data : UserInfo
+        }).then(res => {
+            console.log(res.data)
+            alert('회원가입 완료!')
+            navigate('/loginpage')
+        })
+          .catch(err => {console.log(err.data)})
+        
+        
+    }
+    return(
+        <div className="FreelancerUser">
+            <div className="FreelancerUserinfobox">
+                <div className="FreelancerUserinfobox-titlebox">
+                    <h1 className="FreelancerUserinfobox-titlebox__title">기본정보</h1>
+                </div>
+                <div className="joinbox-countbox">
+                    <h3 className="joinbox-countbox__countpage">3 / 3</h3>
+                </div>
+                <form className="FreelancerUserinfobox-joinform">
+                    <div className="joinform-joinbody">
+                        <div className="joinform-joinbody__joinbox">
+                            <label className="joinform-joinbody__joinbox--label">성명</label>
+                            <input type='text' className="joinform-joinbody__joinbox--userinfoFreelancer" placeholder="성함을 입력해주세요." onChange={InputName}/>
+                        </div>
+                        <div className="joinform-joinbody__joinbox">
+                            <label className="joinform-joinbody__joinbox--label">회원 아이디</label>
+                            <input type='text' className="joinform-joinbody__joinbox--userinfoId" placeholder="아이디를 입력하세요." onChange={InputId}/>
+                        </div>
+                        <div className="joinform-joinbody__joinbox">
+                            <label className="joinform-joinbody__joinbox--label">비밀번호</label>
+                            <input type='password' className="joinform-joinbody__joinbox--userinfoPw" placeholder="비밀번호를 입력하세요." onChange={InputPw}/>
+                        </div>
+                        <div className="joinform-joinbody__joinbox">
+                            <label className="joinform-joinbody__joinbox--label">비밀번호 확인</label>
+                            <input type='text' className="joinform-joinbody__joinbox--userinfoPwchk" placeholder="비밀번호를 다시 입력해주세요." onChange={InputPwchk}/>
+                        </div>
+                        <div className="joinform-joinbody__joinbox">
+                            <label className="joinform-joinbody__joinbox--label">이메일</label>
+                            <input type='email' className="joinform-joinbody__joinbox--userinfoEmail" placeholder="이메일을 입력하세요." onChange={InputEmail}/>
+                        </div>
+                        <div className="joinform-joinbody__joinbox">
+                            <label className="joinform-joinbody__joinbox--label">휴대폰</label>
+                            <input type='text' className="joinform-joinbody__joinbox--userinfoTel" placeholder="전화번호를 입력하세요." onChange={InputPhonenum}/>
+                        </div>
+                        <div className="joinform-joinbody__joinbox">
+                            <label className="joinform-joinbody__joinbox--label">생년월일</label>
+                            <input type='date' className="joinform-joinbody__joinbox--userinfoDamdang" placeholder="이름 / 직책" onChange={InputBirth}/>
+                        </div>
+                    </div>
+                    <input type="submit" value={'회원가입'} onClick={JoinComplete}/>
+                </form>
+            </div>
+        </div>
+    )
 }
