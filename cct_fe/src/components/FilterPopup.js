500303316c5e44460c49d34973b4728b2f9f193a
PBEM22 <dlacofbs1229@gmail.com>
2023-09-14 14:12:48 +0900

    내 이력서 보기 업데이트

--- cct_fe/src/components/FilterPopup.js
+++ cct_fe/src/components/FilterPopup.js
@@ -3,12 +3,12 @@ import React, {useState} from 'react';
 import Modal from 'react-modal';
 
 export default function FilterPopup({setFilter, filter}) {
-    
-    const closeFilterPopup = () => {
+    const [java, setJava] = useState(false);
+    const closeFilterPopup = () => {    // 모달창 없애기
         setFilter(false);
     }
 
-    const customModalStyles = {
+    const customModalStyles = {     // 모달창 디자인
         overlay: {
           backgroundColor: " rgba(0, 0, 0, 0.4)",
           width: "100%",
@@ -46,18 +46,26 @@ export default function FilterPopup({setFilter, filter}) {
             <div className='filter-popup-title'>필터 추가하기</div>     {/* 필터 제목*/}
             <div className='filter-popup-career'>                    {/* 경력 파트 */}
                 <div className='filter-popup-career-text'>경력</div>    {/* 경력 파트 제목*/}
-                <div className='filter-popup-career-btn'>            {/*경력 선택 버튼*/}
-                    <button className='filter-popup-career-1'>1년 이하</button> {/* 1년*/}
-                    <button className='filter-popup-career-3'>3년 이하</button> {/* 3년*/}
-                    <button className='filter-popup-career-5'>5년 이하</button> {/* 5년*/}
+                <div className='filter-popup-career-select'>            {/*경력 선택 버튼*/}
+                <select className='filter-popup-career-option'>
+                    <option className='filter-popup-career-1'>1년 이하</option>
+                    <option className='filter-popup-career-3'>3년 이하</option>
+                    <option className='filter-popup-career-5'>5년 이하</option>
+                </select>
+                
                 </div>
             </div>
-
+            
             <div className='filter-popup-spec'>         {/* 스펙 부분 */}
                 <div className='filter-popup-spec-title'>기술</div> {/* 스펙 제목 */}
                 <div className='filter-popup-spec-btn'>     {/* 스펙 버튼 */}
-                    <button className='filter-popup-spec-java'>JAVA</button>    {/* 자바 버튼*/}
-                    <button className='filter-popup-spec-spring'>SPRING</button>    {/* 스프링 버튼 */}
+                    JAVA
+                    <input className='filter-popup-spec-java'
+                        type='checkbox'
+                        
+                        ></input>
+                     {/* <button className='filter-popup-spec-java'>JAVA</button>    자바 버튼  */}
+                     {/* <button className='filter-popup-spec-spring'>SPRING</button>    스프링 버튼  */}
                 </div>
             </div>
 
--- cct_fe/src/components/ResumePopup.js
+++ cct_fe/src/components/ResumePopup.js
@@ -1,8 +1,48 @@
 import Modal from 'react-modal';
+import React from 'react';
+import { useEffect, useState} from 'react';
+import axios from 'axios';
 
 export default function ResumePopup ({setPopup, popup}) {
-    // 팝업 끄기
-    const closePopup = () => {
+    const [myList, setMyList] = useState([]);
+    const [user, setUser] = useState('');
+
+    const usid = (user) => {
+        setUser(user);  // 서버로 받은 유저 아이디 저장
+    }
+
+    useEffect(() => {
+        const userIdCome = async () => {
+            try {
+               const res = await axios.get(`//localhost:8080/`) 
+            }
+        }
+
+        //리스트 불러오기
+        const myResumeList = async () => {
+            try { 
+                const response = await axios.get(`//localhost:8080/resume/${userid}`)
+                const data = response.data;	
+                
+                console.log(data);
+                // 데이터에서 필요한 값 추출
+        const extractedData = data.map((item) => ({
+            userId: item.userId,    // 리스트 작성자 이름
+            projectList: item.projectList,  // 리스트 작성자 프로젝트 리스트
+            stack: item.stack,  // 작성자 기술
+            period: item.period // 작성자 경력
+          }));
+  
+          console.log(extractedData); // 추출된 데이터 확인
+  
+             } catch(error) {
+                console.error("에러", error);
+             } 
+            };
+            myResumeList();
+    }, []);
+
+    const closePopup = () => {  // 팝업 끄기
         setPopup(false);
     };
     
@@ -40,13 +80,30 @@ export default function ResumePopup ({setPopup, popup}) {
         style={customModalStyles}>
         <div className="popup">
             <button className="close-btn" onClick={closePopup}>
-                X
+                창 닫기
             </button>
-            <div className="resume-list-box">
+            
+            {myList.map((item) => (
+                <div key={item.userId} className='resume-list-box'>
+                    <div className='resume-title'>안녕하세요. {item.userId}입니다.</div>
+
+                <p className='resume-spec'>{item.stack}</p>
+                <p className='resume-detail'>{item.period}</p>
+
+                <ul>
+                    {item.projectList.map((project, index) => (
+                        <li key={index}>{project}</li>
+                    ))}
+                </ul>
+
+                </div>
+            ))}
+
+             {/* <div className="resume-list-box">
                 <div className="resume-title"></div>
                 <div className="resume-content"></div>
                 <div className="resume-company"></div>
-            </div>
+            </div> */}
         </div>
         </Modal>
     )
