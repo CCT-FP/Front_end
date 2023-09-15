48f05e8c9923c98ba0eda31b1760bdabda17b0d1
PBEM22 <dlacofbs1229@gmail.com>
2023-09-15 17:49:43 +0900

    필터 팝업 기능 완성

--- cct_fe/src/App.js
+++ cct_fe/src/App.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import axios from 'axios';
-=======
 import './css/App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Mainpage from './pages/Mainpage';
@@ -13,7 +11,6 @@ import Mypage from './pages/Mypage';
 import Agreepage from './pages/Agreepage';
 import Userinfo from './pages/Userinfo';
 import WriteResume from './pages/WriteResume';
->>>>>>> 2b49a08a2c2235f08b777cf679eac9201f9d191a
 
 function App() {
   const [data, setData] = useState(null);
@@ -66,25 +63,23 @@ function App() {
   };
 
   return (
-<<<<<<< HEAD
-    <div>
-      <button onClick={resumeList}>전체 이력서 리스트</button> <br></br>
-      <button onClick={resumeCheck}>본인 이력서 조회</button><br></br>
-      <button onClick={resumeSave}>이력서 저장</button><br></br>
+    // <div>
+    //   <button onClick={resumeList}>전체 이력서 리스트</button> <br></br>
+    //   <button onClick={resumeCheck}>본인 이력서 조회</button><br></br>
+    //   <button onClick={resumeSave}>이력서 저장</button><br></br>
 
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>Error occurred: {error}</p>
-      ) : data ? (
-        <ul>
-          {data.map(item => (
-            <p><li key={item.id}>{item.id}. {item.title}</li></p> 
-          ))}
-        </ul>
-      ) : null}
-    </div>
-=======
+    //   {loading ? (
+    //     <p>Loading...</p>
+    //   ) : error ? (
+    //     <p>Error occurred: {error}</p>
+    //   ) : data ? (
+    //     <ul>
+    //       {data.map(item => (
+    //         <p><li key={item.id}>{item.id}. {item.title}</li></p> 
+    //       ))}
+    //     </ul>
+    //   ) : null}
+    // </div>
     <BrowserRouter>
         <Headerbar/>
         <Routes>
@@ -98,7 +93,6 @@ function App() {
           <Route path='/resume/writeresume' element={<WriteResume/>}/>
         </Routes>
     </BrowserRouter>
->>>>>>> 2b49a08a2c2235f08b777cf679eac9201f9d191a
   );
 }
 
