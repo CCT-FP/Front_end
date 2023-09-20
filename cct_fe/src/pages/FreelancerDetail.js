3cf6daf01033bb99ef32b9523e43aa025b569a02
PBEM22 <dlacofbs1229@gmail.com>
2023-09-21 01:32:16 +0900

    끄앙

--- cct_fe/src/pages/FreelancerDetail.js
+++ cct_fe/src/pages/FreelancerDetail.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useId, useState} from "react";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import Note from "./Note";
@@ -7,18 +7,16 @@ import '../css/FreelancerDetail.css'
 export default function FreelancerDetail(){
     const [isSameid, setIsSameid] = useState(false)
     const [openpop, setOpenpop] = useState(false)
-    const location = useLocation()
+    const location = useLocation()      // 받아주려면 필수
     const navigate = useNavigate()
-    const userId = location.state.userid
-    const id = window.localStorage.getItem('id')
-    useEffect(() => {
+    const userId = location.state.userid    // userid 받기
+    const id = window.localStorage.getItem('id')    // 로그인된 id 받기
+    useEffect(() => {   
         axios({
             method : 'get',
             url : `//localhost:8080/resume/${userId}`,
         })
-        .then(res => {
-            
-        })
+        .then(res => console.log(res))
         .catch(err => console.log(err))
         if(userId === id){      // 로그인된 id와 같다면, 쪽지 생성 안하기
             setIsSameid(true)
@@ -28,25 +26,11 @@ export default function FreelancerDetail(){
    
 
     return(
-        <div className="freelancerDetail">
-            <h2 className="introducefreelancer">{`안녕하세요. ${1}`}</h2>
+        <div className="Freelancerdetail">
+            <button className="MovetoNotebtn" onClick={() => {
+                setOpenpop(true)
+            }}></button>
+            <Note isOpen = {openpop} receiver={userId} setOpenpop={setOpenpop}></Note>
         </div>
-
-        // <div className="Freelancerdetail">
-        //     {
-        //         isSameid ?  <>
-        //         <div>
-        //             <button className="MovetoNotebtn" onClick={() => {
-        //                 setOpenpop(true)
-        //             }}></button>
-        //         </div>
-        //         </> 
-        //         :
-        //         <>
-        //             <div></div>
-        //        </>
-        //     }
-        //     <Note isOpen = {openpop} receiver={userId} setOpenpop={setOpenpop}></Note>
-        //</div>
     )
 }
